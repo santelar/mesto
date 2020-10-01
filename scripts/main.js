1f4f97693acedba49f866b8bfbf2e871cd1051dc
Alexander Ponomarenko <madi-al@yandex.ru>
2020-10-01 16:59:34 +0300

    На ревью - исправлен JS

--- scripts/main.js
+++ scripts/main.js
@@ -3,43 +3,46 @@ let popupOpenButton = document.querySelector('.profile__edit');
 let popupCloseButton = popup.querySelector('.popup__close');
 let name = document.querySelector('.profile__name');
 let description = document.querySelector('.profile__description');
-let nameInput = popup.querySelector('.popup__edit-name');
-let descriptionInput = popup.querySelector('.popup__edit-description');
+let nameInput = popup.querySelector('.popup__edit_name');
+let descriptionInput = popup.querySelector('.popup__edit_description');
 let popupSaveButton = popup.querySelector('.popup__save');
 
-// Функция переключения класса попапа
-let popupToggle = function () {
-  popup.classList.toggle('popup_opened');
+// Функция переноса в попап данных профайла при открытии попапа //
+function formOpenHandler () {
+  nameInput.value = document.getElementsByClassName("profile__name")['0'].textContent;
+  descriptionInput.value = document.getElementsByClassName("profile__description")['0'].textContent;
 }
 
-// Функция закрытия попапа при клике вне попапа
-let closePopup = function (event){
+// Функция переключения класса попапа //
+let popupToggle = function () {
+  if (popup.classList.contains('popup_opened') !== true) {
+    popup.classList.toggle('popup_opened');
+    formOpenHandler ();
+  } else {
+    popup.classList.toggle('popup_opened');
+  }
+}
+
+// Функция закрытия попапа при клике вне попапа //
+let closePopup = function (event) {
   if (event.target !== event.currentTarget) return
   popupToggle(event);
 }
 
-// Функция переноса в попап данных профайла при открытии попапа
-function formOpenHandler (evt) {
-  evt.preventDefault();
-  nameInput.value = document.getElementsByTagName("h1")['0'].textContent;
-  descriptionInput.value = document.getElementsByTagName("h3")['0'].textContent;
+// Функция сохранения //
+function formSubmitHandler (event) {
+  event.preventDefault();
+  name.textContent = nameInput.value;
+  description.textContent = descriptionInput.value;
+  popupToggle ();
 }
 
-// Обработчик открытия попапа
-popupOpenButton.addEventListener('click', formOpenHandler);
+// Обработчик открытия попапа //
 popupOpenButton.addEventListener('click', popupToggle);
 
-// Обработчик закрытия попапа без сохранения
+// Обработчик закрытия попапа без сохранения //
 popupCloseButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', closePopup);
 
-// Функция сохранения
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  name.textContent = nameInput.value;
-  description.textContent = descriptionInput.value;
-}
-
-// Обработчик сохранения
-popupSaveButton.addEventListener('click', formSubmitHandler);
-popupSaveButton.addEventListener('click', closePopup);
+// Обработчик сохранения //
+popup.addEventListener('submit', formSubmitHandler);
