8912d22ea3204c16c694be9fd5c6821cf0307aa0
Alexander Ponomarenko <madi-al@yandex.ru>
2020-10-14 22:16:31 +0300

    Second popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,49 @@
 let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__edit');
-let popupCloseButton = popup.querySelector('.popup__close');
+let popupProfile = document.querySelector('.popup_profile');
+let popupCard = document.querySelector('.popup_card');
+let profileOpenButton = document.querySelector('.profile__edit');
+let popupClose = document.querySelector('.popup__close');
+let CardOpenButton = document.querySelector('.button_add_card');
 let name = document.querySelector('.profile__name');
 let description = document.querySelector('.profile__description');
-let nameInput = popup.querySelector('.popup__name');
-let descriptionInput = popup.querySelector('.popup__description');
+let nameInput = document.querySelector('.popup__name');
+let descriptionInput = document.querySelector('.popup__description');
+
+const placesCards = [
+  { name: 'Москва', link: './images/moscow.jpg'},
+  { name: 'Байкал', link: './images/baykal.jpg'},
+  { name: 'Екатеринбург', link: './images/ekaterinburg.jpg'},
+  { name: 'Камчатка', link: './images/kamchatka.jpg'},
+  { name: 'Роща улица Желаний', link: './images/roscha_ulitsa_zhelaniy.jpg'},
+  { name: 'Карелия', link: './images/karelia.jpg'}
+]; 
+const template = document.querySelector('.template');
+let elements = document.querySelector('.elements');
+
+const getItems = (data) => {
+  const card = template.content.cloneNode(true);
+  card.querySelector('.card__title').innerText = data.name;
+  card.querySelector('.card__image').setAttribute('src', data.link);
+  card.querySelector('.card__image').setAttribute('alt', data.name);
+  elements.prepend(card);
+  console.log(card);
+};
+
+const renderList = () => {
+  const items = placesCards.map(element => getItems(element));
+  
+};
+
+renderList ();
+
+
+
+
+
+
+
+
+
 
 // Функция переноса в попап данных профайла при открытии попапа //
 function formOpenHandler () {
@@ -28,11 +67,15 @@ function formSubmitHandler (event) {
   popupToggle ();
 }
 
+CardOpenButton.addEventListener('click', popupToggle);
+
 // Обработчик открытия попапа //
-popupOpenButton.addEventListener('click', popupToggle);
+profileOpenButton.addEventListener('click', popupToggle);
 
 // Обработчик закрытия попапа без сохранения //
-popupCloseButton.addEventListener('click', popupToggle);
+popupClose.addEventListener('click', popupToggle);
 
 // Обработчик сохранения //
-popup.addEventListener('submit', formSubmitHandler);
+popupProfile.addEventListener('submit', formSubmitHandler);
+
+
