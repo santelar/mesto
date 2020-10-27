f66a8c062bb3ce7e3b99173baa94c7d3816a19ad
Alexander Ponomarenko <madi-al@yandex.ru>
2020-10-27 15:04:58 +0300

    for change to popup__input

---
+++ scripts/forms.js
@@ -0,0 +1,23 @@
+const newPlaceForm = document.querySelector('.popup__form');
+newPlaceForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    console.log('!!!!!!!!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//enableValidation({
+  //  formSelector: '.popup__form',
+    //buttonSelector: '.popup__button'
+//});
