8b19c18523dc4c7466d2fe0f1f4ad0bb503f0574
santelar <66886559+santelar@users.noreply.github.com>
2020-11-22 23:42:05 +0300

    Update initialItems.js

--- src/scripts/initialItems.js
+++ src/scripts/initialItems.js
@@ -1,7 +1,7 @@
 //Массив картинок//
 export const initialItems = [
   { name: 'Москва', link: './src/images/moscow.jpg'},
-  { name: 'Байкал', link: './src/images/moscow.jpg'},
+  { name: 'БайкалБайкал', link: './src/images/moscow.jpg'},
   // { name: 'Байкал', link: './src/images/baykal.jpg'},
   { name: 'Владивосток', link: './src/images/vladivostok.jpg'},
   { name: 'Санкт-Петербург', link: './src/images/piter.jpg'},
