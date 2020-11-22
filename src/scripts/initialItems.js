608904fbc891b6611b25f2522609123bde078842
santelar <66886559+santelar@users.noreply.github.com>
2020-11-22 23:44:01 +0300

    Update initialItems.js

--- src/scripts/initialItems.js
+++ src/scripts/initialItems.js
@@ -2,7 +2,7 @@
 export const initialItems = [
   { name: 'Москва', link: './src/images/moscow.jpg'},
   { name: 'БайкалБайкал', link: './src/images/moscow.jpg'},
-  // { name: 'Байкал', link: './src/images/baykal.jpg'},
+  { name: 'Байкал', link: './src/images/baykal.jpg'},
   { name: 'Владивосток', link: './src/images/vladivostok.jpg'},
   { name: 'Санкт-Петербург', link: './src/images/piter.jpg'},
   { name: 'Роща улица Желаний', link: './src/images/roscha_ulitsa_zhelaniy.jpg'},
