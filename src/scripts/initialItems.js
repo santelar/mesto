b604b675d6e77f6f93297bb32422c75aa19693a9
Alexander Ponomarenko <madi-al@yandex.ru>
2020-11-22 23:47:55 +0300

    qwert

--- src/scripts/initialItems.js
+++ src/scripts/initialItems.js
@@ -1,8 +1,7 @@
 //Массив картинок//
 export const initialItems = [
   { name: 'Москва', link: './src/images/moscow.jpg'},
-  { name: 'БайкалБайкал', link: './src/images/moscow.jpg'},
-  { name: 'Байкал', link: './src/images/baykal.jpg'},
+  { name: 'Байкал', link: './src/images/baikal.jpg'},
   { name: 'Владивосток', link: './src/images/vladivostok.jpg'},
   { name: 'Санкт-Петербург', link: './src/images/piter.jpg'},
   { name: 'Роща улица Желаний', link: './src/images/roscha_ulitsa_zhelaniy.jpg'},
